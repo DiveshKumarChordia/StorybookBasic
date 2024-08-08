eb665181241859d86bc466e3ff83a61015defb5b
Divesh Kumar Chordia <diveshkumar.chordia@contentstack.com>
2024-08-08 17:03:06 +0530

    First Chapter

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        'storybook/internal/manager-api',
+        'storybook/internal/components',
+        'storybook/internal/theming',
+        'storybook/internal/core-events',
+      ],
+    },
+  },
   plugins: [react()],
 })
